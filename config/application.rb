aa3dfc0204b7890da81e24c446a350e7c989c8c2
saitousl <saito@usl.cs.tut.ac.jp>
2017-08-06 06:02:16 +0000

    show update_time in jp

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,5 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Asia/Tokyo'
-    config.active_record.default_timezone = :local
   end
 end
